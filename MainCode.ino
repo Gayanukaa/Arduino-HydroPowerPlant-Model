1db65ef15cbd6bc8ebc5eebf9ccdcb0fac392b3b
Gayanuka Amarasuriya <gayanukaamarasuriya@gmail.com>
2023-07-26 13:36:54 +0530

    Create MainCode.ino

---
+++ MainCode.ino
@@ -0,0 +1,4 @@
+//https://www.tinkercad.com/things/2JgiVnnB5oi-smooth-hango-waasa/editel?tenant=circuits
+
+//arduino code for four channel four way relay  where each channel is connected to 4 LEDs in parallel and pins of arduino board utilized four 4 relays are pins 13,12,11,10. Realy should work to produce a wave motion
+//For the 16 LEDs connected pin 10 connected to LEDs 1,5,9,13; pin 11 connected to LEDs 2,6,10,14; pin 12 connected to LEDs 3,7,11,15; pin 13 connected to LEDs 4,8,12,16
