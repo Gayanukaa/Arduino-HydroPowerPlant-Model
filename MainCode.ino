9e2b8b0493b7e457f9fea89f3c78cbafca43fa2c
Gayanuka Amarasuriya <gayanukaamarasuriya@gmail.com>
2023-07-26 18:36:56 +0530

    Wave code added

--- MainCode.ino
+++ MainCode.ino
@@ -2,3 +2,42 @@
 
 //arduino code for four channel four way relay  where each channel is connected to 4 LEDs in parallel and pins of arduino board utilized four 4 relays are pins 13,12,11,10. Realy should work to produce a wave motion
 //For the 16 LEDs connected pin 10 connected to LEDs 1,5,9,13; pin 11 connected to LEDs 2,6,10,14; pin 12 connected to LEDs 3,7,11,15; pin 13 connected to LEDs 4,8,12,16
+
+int relay1 = 13;
+int relay2 = 12;
+int relay3 = 11;
+int relay4 = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+  pinMode(relay3, OUTPUT);
+  pinMode(relay4, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(relay1, HIGH);
+  digitalWrite(relay2, LOW);
+  digitalWrite(relay3, LOW);
+  digitalWrite(relay4, LOW);
+  delay(1000);
+  digitalWrite(relay1, LOW);
+  digitalWrite(relay2, HIGH);
+  digitalWrite(relay3, LOW);
+  digitalWrite(relay4, LOW);
+  delay(1000);
+  digitalWrite(relay1, LOW);
+  digitalWrite(relay2, LOW);
+  digitalWrite(relay3, HIGH);
+  digitalWrite(relay4, LOW);
+  delay(1000);
+  digitalWrite(relay1, LOW);
+  digitalWrite(relay2, LOW);
+  digitalWrite(relay3, LOW);
+  digitalWrite(relay4, HIGH);
+  delay(1000);
+}
+
+// Path: MainCode.ino
