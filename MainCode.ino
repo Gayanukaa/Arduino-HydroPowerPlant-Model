df855c8dce75c2fcc51dfa53a4e2712f64b744e2
Gayanuka Amarasuriya <gayanukaamarasuriya@gmail.com>
2023-07-26 19:31:30 +0530

    Inlet Valve Code

--- MainCode.ino
+++ MainCode.ino
@@ -3,41 +3,79 @@
 //arduino code for four channel four way relay  where each channel is connected to 4 LEDs in parallel and pins of arduino board utilized four 4 relays are pins 13,12,11,10. Realy should work to produce a wave motion
 //For the 16 LEDs connected pin 10 connected to LEDs 1,5,9,13; pin 11 connected to LEDs 2,6,10,14; pin 12 connected to LEDs 3,7,11,15; pin 13 connected to LEDs 4,8,12,16
 
-int relay1 = 13;
-int relay2 = 12;
-int relay3 = 11;
-int relay4 = 10;
+
+int outRelay1 = 13;
+int outRelay2 = 12;
+int outRelay3 = 11;
+int outRelay4 = 10;
+
+int inRelay1 = 7;
+int inRelay2 = 6;
+int inRelay3 = 5;
+int inRelay4 = 4;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(relay1, OUTPUT);
-  pinMode(relay2, OUTPUT);
-  pinMode(relay3, OUTPUT);
-  pinMode(relay4, OUTPUT);
+    // put your setup code here, to run once:
+    pinMode(outRelay1, OUTPUT);
+    pinMode(outRelay2, OUTPUT);
+    pinMode(outRelay3, OUTPUT);
+    pinMode(outRelay4, OUTPUT);
+
+    pinMode(inRelay1, OUTPUT); 
+    pinMode(inRelay2, OUTPUT);
+    pinMode(inRelay3, OUTPUT);
+    pinMode(inRelay4, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(relay1, LOW);
-  digitalWrite(relay2, LOW);
-  digitalWrite(relay3, LOW);
-  digitalWrite(relay4, HIGH);
-  delay(500);
-  digitalWrite(relay1, LOW);
-  digitalWrite(relay2, LOW);
-  digitalWrite(relay3, HIGH);
-  digitalWrite(relay4, LOW);
-  delay(500);
-  digitalWrite(relay1, LOW);
-  digitalWrite(relay2, HIGH);
-  digitalWrite(relay3, LOW);
-  digitalWrite(relay4, LOW);
-  delay(500);
-  digitalWrite(relay1, HIGH);
-  digitalWrite(relay2, LOW);
-  digitalWrite(relay3, LOW);
-  digitalWrite(relay4, LOW);
-  delay(500);
+    // put your main code here, to run repeatedl
+    digitalWrite(outRelay1, LOW);
+    digitalWrite(outRelay2, LOW);
+    digitalWrite(outRelay3, LOW);
+    digitalWrite(outRelay4, HIGH);
+
+    digitalWrite(inRelay1, LOW);
+    digitalWrite(inRelay2, LOW);
+    digitalWrite(inRelay3, LOW);
+    digitalWrite(inRelay4, HIGH);
+
+    delay(500);
+
+    digitalWrite(outRelay1, LOW);
+    digitalWrite(outRelay2, LOW);
+    digitalWrite(outRelay3, HIGH);
+    digitalWrite(outRelay4, LOW);
+
+    digitalWrite(inRelay1, LOW);
+    digitalWrite(inRelay2, LOW);
+    digitalWrite(inRelay3, HIGH);
+    digitalWrite(inRelay4, LOW);
+
+    delay(500);
+
+    digitalWrite(outRelay1, LOW);
+    digitalWrite(outRelay2, HIGH);
+    digitalWrite(outRelay3, LOW);
+    digitalWrite(outRelay4, LOW);
+
+    digitalWrite(inRelay1, LOW);
+    digitalWrite(inRelay2, HIGH);
+    digitalWrite(inRelay3, LOW);
+    digitalWrite(inRelay4, LOW);
+
+    delay(500);
+
+    digitalWrite(outRelay1, HIGH);
+    digitalWrite(outRelay2, LOW);
+    digitalWrite(outRelay3, LOW);
+    digitalWrite(outRelay4, LOW);
+
+    digitalWrite(inRelay1, HIGH);
+    digitalWrite(inRelay2, LOW);
+    digitalWrite(inRelay3, LOW);
+    digitalWrite(inRelay4, LOW);
+
+    delay(500);
 }
 
 // Path: MainCode.ino
